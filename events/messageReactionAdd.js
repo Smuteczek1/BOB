e45e1cadf174dc8423c6d51be6ef8d3f58125719
Sadness <127046921+Smuteczek1@users.noreply.github.com>
2026-08-15 22:07:41 +0200

    Update messageReactionAdd.js

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,8 +1,10 @@
 const { handleReactionToggle } = require('../utils/rolePanels');
+const { handleVerifyReaction } = require('../utils/verification');
 
 module.exports = {
   name: 'messageReactionAdd',
   async execute(reaction, user) {
     await handleReactionToggle(reaction, user, true);
+    await handleVerifyReaction(reaction, user);
   },
 };
