980edaf99ef6f7571b79204696bf26dd8f229a04
Sadness <127046921+Smuteczek1@users.noreply.github.com>
2026-08-15 23:54:43 +0200

    Remove verification reaction handling
    Removed the call to handleVerifyReaction in the messageReactionAdd event.

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,10 +1,8 @@
 const { handleReactionToggle } = require('../utils/rolePanels');
-const { handleVerifyReaction } = require('../utils/verification');
 
 module.exports = {
   name: 'messageReactionAdd',
   async execute(reaction, user) {
     await handleReactionToggle(reaction, user, true);
-    await handleVerifyReaction(reaction, user);
   },
 };
